ce7f94c9880773fbce597e49a1f4288e04e1b7e0
Fabián Montealegre <fabimc@gmail.com>
2022-11-13 22:45:54 -0600

    cleans up menu

--- routes/index.tsx
+++ routes/index.tsx
@@ -148,20 +148,6 @@ export default function Home({ data }: PageProps<GameStores>) {
     <div>
       <nav id='header' class='w-full z-30 top-0 py-1'>
         <div class='w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3'>
-          <label for='menu-toggle' class='cursor-pointer md:hidden block'>
-            <svg
-              class='fill-current text-gray-900'
-              xmlns='http://www.w3.org/2000/svg'
-              width='20'
-              height='20'
-              viewBox='0 0 20 20'
-            >
-              <title>menu</title>
-              <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'></path>
-            </svg>
-          </label>
-          <input class='hidden' type='checkbox' id='menu-toggle' />
-
           <div
             class='hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1'
             id='menu'
