84f0431c24a09dd70ef17b195f2c486f2995c708
Fabián Montealegre <fabimc@gmail.com>
2023-03-24 12:02:11 -0600

    updates icon

--- routes/index.tsx
+++ routes/index.tsx
@@ -178,16 +178,7 @@ export default function Home({ data }: PageProps<GameStores>) {
               class='flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl '
               href=''
             >
-              <svg
-                class='fill-current text-gray-800 mr-2'
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-              >
-                <path d='M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z' />
-              </svg>
-              Game Orb
+              🔮 Game Orb
             </a>
           </div>
 
@@ -236,13 +227,6 @@ export default function Home({ data }: PageProps<GameStores>) {
                 <img class='hover:shadow-lg' src={game.image} />
                 <div class='pt-3 flex items-center justify-between'>
                   <p class=''>{game.name}</p>
-                  {/* <svg
-                class="h-6 w-6 fill-current text-gray-500 hover:text-black"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
-              </svg> */}
                 </div>
                 <p class='pt-1 text-gray-900'>{game.price}</p>
               </a>
@@ -281,13 +265,6 @@ export default function Home({ data }: PageProps<GameStores>) {
                 <img class='hover:shadow-lg' src={game.image} />
                 <div class='pt-3 flex items-center justify-between'>
                   <p class=''>{game.name}</p>
-                  {/* <svg
-                class="h-6 w-6 fill-current text-gray-500 hover:text-black"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
-              </svg> */}
                 </div>
                 <p class='pt-1 text-gray-900'>{game.price}</p>
               </a>
