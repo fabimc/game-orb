96192d4d5d86a6e6f3593a902a54e62dd78745ed
Fabián Montealegre <fabimc@gmail.com>
2023-08-10 17:00:54 -0600

    fixes super rare

--- routes/api/superrare.ts
+++ routes/api/superrare.ts
@@ -3,7 +3,8 @@ import { cheerio } from 'https://deno.land/x/denocheerio@1.0.0/mod.ts'
 import { Game } from '../../types/game.type.ts'
 
 const getGames = async (siteUrl: string) => {
-  const superRareGamesResponse = await fetch(`${siteUrl}/collections/all`)
+  const superRareGamesResponse = await fetch(`${siteUrl}/collections/featured`)
+
   if (superRareGamesResponse.status === 404) {
     return []
   }
@@ -15,22 +16,24 @@ const getGames = async (siteUrl: string) => {
 
 const mapGames = (webpage: string, siteUrl: string): Game[] => {
   const $ = cheerio.load(webpage)
-  const gamesSelector = $('.product-collection :has(.img-wrap > .img-container img.rotateShadow[alt^="SRG#"])')
+  const gamesSelector = $('.product-card')
 
   const games = gamesSelector
     .map((_i: number, el: cheerio.Element) => {
-      const imageSelector = $(el).find('.img-wrap > .img-container img.rotateShadow[alt^="SRG#"]').first()
-      const name = imageSelector.attr('alt') || ''
+      const imageSelector = $(el).find('img').first()
+      const name = $(el).find('.badge--sold_out').first().text() || imageSelector.attr('alt') || ''
       const image = imageSelector.attr('src') || ''
-      const url = siteUrl + $(el).find('.img-wrap > .img-container > a').first().attr('href') || ''
-      const price = $(el).parent().parent().find('.money').first().text() || ''
+      const url = siteUrl + $(el).find('a').first().attr('href') || ''
+      const price = $(el).find('.money').first().text() || ''
 
       return { name, image, url, price }
     })
     .get()
 
+    const singles = games.filter((game) => game.name.startsWith('SRG#'))
+
     // removes duplicates
-    return [...new Map(games.map((game) => [game.name, game])).values()];
+    return [...new Map(singles.map((game) => [game.name, game])).values()];
 }
 
 export const handler = async (_req: Request, _ctx: HandlerContext): Promise<Response> => {
