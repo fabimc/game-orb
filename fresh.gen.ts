f134a97b9c48f38941b323cb22a34ec60b296955
Fabián Montealegre <fabimc@gmail.com>
2024-03-11 11:07:31 -0600

    adds middleware

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $_middleware from "./routes/_middleware.ts";
 import * as $api_name_ from "./routes/api/[name].ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
@@ -14,6 +15,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/_middleware.ts": $_middleware,
     "./routes/api/[name].ts": $api_name_,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
